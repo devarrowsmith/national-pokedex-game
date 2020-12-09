70ec964a614fef6026b522ea453dfe3f5e3d7225
GummiCode <gummicodeblog@gmail.com>
2020-12-09 19:47:17 +0000

    Slightly modified the bounce animation of the BlockSprite component.

--- src/components/BlockSprite.js
+++ src/components/BlockSprite.js
@@ -15,7 +15,7 @@ const StyledSprite = styled.img`
         10%  { transform: scale(1.1,.9)   translateY(0); }
         30%  { transform: scale(.9,1.1)   translateY(-20px); }
         50%  { transform: scale(1.05,.95) translateY(0); }
-        57%  { transform: scale(1,1)      translateY(-7px); }
+        57%  { transform: scale(1,1)      translateY(-2px); }
         64%  { transform: scale(1,1)      translateY(0); }
         100% { transform: scale(1,1)      translateY(0); }
     };
