dd7a92dac526b911f48274b506e8895cb25eb874
GummiCode <gummicodeblog@gmail.com>
2020-12-09 19:45:48 +0000

    Animated the Victini blocks that show twhen the player wins using a conditional prop-based animation in the CSS styling.

--- src/components/BlockSprite.js
+++ src/components/BlockSprite.js
@@ -4,19 +4,33 @@ import styled from 'styled-components';
 
 const StyledSprite = styled.img`
   height: 70%;
-  background-color: ${(prop) => (prop.className === 'victini' ? 'purple' : 'transparent')};
+  animation-duration: 2s;
+  animation-iteration-count: infinite;
+  transform-origin: bottom;
+  animation-name: ${(prop) => (prop.className === 'victini' ? 'bounce' : 'none')};
+  animation-timing-function: cubic-bezier(0.075, 0.82, 0.165, 1);
 
+  @keyframes bounce {
+    0%   { transform: scale(1,1)      translateY(0); }
+        10%  { transform: scale(1.1,.9)   translateY(0); }
+        30%  { transform: scale(.9,1.1)   translateY(-20px); }
+        50%  { transform: scale(1.05,.95) translateY(0); }
+        57%  { transform: scale(1,1)      translateY(-7px); }
+        64%  { transform: scale(1,1)      translateY(0); }
+        100% { transform: scale(1,1)      translateY(0); }
+    };
 `;
 
 const BlockSprite = ({ sprite, text, spriteClass }) => {
   console.log(spriteClass);
-  return(
-  <StyledSprite
-    src={sprite}
-    alt={text}
-    className={spriteClass}
-  />
-)};
+  return (
+    <StyledSprite
+      src={sprite}
+      alt={text}
+      className={spriteClass}
+    />
+  );
+};
 
 BlockSprite.propTypes = {
   sprite: PropTypes.string.isRequired,
